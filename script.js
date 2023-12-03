17b6a146da05a046a02368534a8100d81e993a25
letcodewithvineet <finalsolutioncode@gmail.com>
2023-12-03 23:53:47 +0530

    IMDB Clone Push 1

---
+++ script.js
@@ -0,0 +1,49 @@
+const searchInput = document.getElementById('search-input');
+const searchResultsList = document.getElementById('search-results');
+const favouritesList = document.getElementById('favourites-list');
+
+searchInput.addEventListener('input', (event) => {
+    fetchMovies(event.target.value);
+});
+
+function fetchMovies(query) {
+    fetch(`https://www.omdbapi.com/?s=${query}&apikey=6ebaa4c6`)
+        .then((response) => response.json())
+        .then((data) => {
+            searchResultsList.innerHTML = '';
+            data.Search.forEach((movie) => {
+                const listItem = document.createElement('li');
+                listItem.textContent = movie.Title;
+                listItem.id = movie.imdbID;
+
+                const favouriteButton = document.createElement('button');
+                favouriteButton.classList.add('favourite-button');
+                favouriteButton.textContent = 'Favourite';
+                favouriteButton.addEventListener('click', () => addToFavourites(movie));
+
+                listItem.appendChild(favouriteButton);
+                searchResultsList.appendChild(listItem);
+            });
+        });
+}
+
+function addToFavourites(movie) {
+    const favouriteItem = document.createElement('li');
+    favouriteItem.textContent = movie.Title;
+    favouriteItem.id = movie.imdbID;
+
+    const removeFavouriteButton = document.createElement('button');
+    removeFavouriteButton.classList.add('remove-favourite-button');
+    removeFavouriteButton.textContent = 'Remove';
+    removeFavouriteButton.addEventListener('click', () => removeFromFavourites(movie.imdbID));
+
+    favouriteItem.appendChild(removeFavouriteButton);
+    favouritesList.appendChild(favouriteItem);
+
+    searchResultsList.querySelector(`#${movie.imdbID}`).remove();
+}
+
+function removeFromFavourites(imdbID) {
+    const movie = document.getElementById(imdbID);
+    movie.remove();
+}
